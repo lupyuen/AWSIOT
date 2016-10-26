28531b542d5ed21511521302d98c1b2c042fe74a
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2016-10-27 07:56:47 +0800

    Updated update script

--- update/update.sh
+++ update/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /home/pi/TP-IoT
-curl https://raw.githubusercontent.com/lupyuen/RaspberryPiImage/master/home/pi/TP-IoT/read_dht22.py >read_dht22.py
-python read_dht22.py
+####curl https://raw.githubusercontent.com/lupyuen/RaspberryPiImage/master/home/pi/TP-IoT/read_dht22.py >read_dht22.py
+python send_sensor_data.py
 
