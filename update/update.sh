94b99716f6218e7d6fce31b82189b597670b9e1c
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2016-10-27 08:52:12 +0800

    Minor fix

--- update/update.sh
+++ update/update.sh
@@ -2,5 +2,5 @@
 
 cd /home/pi/TP-IoT
 ####curl https://raw.githubusercontent.com/lupyuen/RaspberryPiImage/master/home/pi/TP-IoT/read_dht22.py >read_dht22.py
-python send_sensor_data.py
+python3 send_sensor_data.py
 
